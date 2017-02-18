5d4ff90b7da8c438e26d092bc7100adeab80465a
Chiemeka Ihebom <emekavilla@gmail.com>
2017-02-18 14:58:59 -0500

    Fixed minor bug

--- src/app.js
+++ src/app.js
@@ -1,67 +1,50 @@
 
-// Check to see if correct radio button is selected.
-
-
-
-
-
-
-
-
-// $('input:radio').each(function() {
-//   if($(this).is(':checked')) {
-//   }
-//   else {
-
-//   }
-// });
-
-
-// for (i = 0; i < paths.length; i++) {
-//     if (name[i].checked) {
-
-//     }
-// }
-
 // ********************
 // // Disabled tooltip on line 153 in map.js file.
 // // This removed the display box of answer when clicked.
-// ********************
+function compareCapitalName(countryName, guessedCapital) {
+  // console.log(paths[1].text.match(/\<h1\>(.*?)\<\/h1\>/)[1])
+  paths.map((el, idx) => {
+    if (el.text.match(/\<h1\>(.*?)\<\/h1\>/)[1] === countryName) {
+           console.log(el.text.match(/\<h1\>(.*?)\<\/h1\>/)[1])
+      console.log(idx)
 
+      if (el.name === guessedCapital) {
+        paths[idx].enable = false;
+        alert('you guessed it right!');
+      }
+      else {
+        alert('you guessed it wrong!');
+      }
+      console.log(el);
+    }
+  })
+}
 
-    document.getElementById('button').addEventListener('click', function(e){
+    document.getElementsByTagName("P").innerHTML = ('Submit').addEventListener('click', function(e){
+      let counrtyName = $('h1').text();
+      // console.log(counrtyName);
+
         $('input').each(function(index,element){
-          //console.log(index, element);
+          // console.log(paths[0].text)
           if(element.checked) {
             // $('h1').text();
             paths.forEach((el,i) => {
               const paths_text = el.text.match($('h1').text());
               //console.log(paths_text);
               if (paths_text) {
-                console.log($(`input[name='capital']:checked`));
-                // if(el.name === $('input[name='capital']:checked')) {
-                //     alert('Guessed Right!');
-                // }
+                // console.log(element.value)
+                compareCapitalName(counrtyName, element.value);
               }
             });
-            //console.log(element);
-            //console.log($('input option:checked'));
-            //if ($('input option:checked').value === )
           }
         });
-        // console.log($('input'))
 
-    // function print () {
-    // alert('try again');
-    // }
-});
+     });
 
 
 
 
-//     for(i=0 ; i<paths.length ; i++){
-//   paths[i].addEventListener("click", enable == false);
-// }
 
 
 
